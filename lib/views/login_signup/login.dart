690582f966d962cb5613b90dfe424a30b26e46b9
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-06-17 00:03:17 +0530

    Added signin option and optimized the code by creating a build function for text fields

--- lib/views/login_signup/login.dart
+++ lib/views/login_signup/login.dart
@@ -1,3 +1,4 @@
+import 'package:dine_ease/views/login_signup/signup.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -8,30 +9,98 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  InputDecoration buildInputDecoration(String labelText,IconData icon) {
+    return InputDecoration(
+      border: UnderlineInputBorder(),
+      labelText: 'Enter your username',
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey),
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.blue, width: 2.0),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      prefixIcon: Icon(icon, color: Colors.grey),
+      filled: true,
+      fillColor: Colors.grey.shade100,
+    );
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+    final screen_width=MediaQuery.of(context).size.width;
+    final screen_height=MediaQuery.of(context).size.height;
 
-            TextField(
-              decoration: InputDecoration(labelText: "Email"),
+    final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.black,
+      minimumSize: Size(double.infinity,50),
+      // padding: EdgeInsets.symmetric(horizontal: 100),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Login",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                SizedBox(height: screen_height/15,),
+                TextFormField(
+                  decoration:  buildInputDecoration("Enter your email", Icons.email),
+                ),
+                SizedBox(height: 30,),
+                TextFormField(
+                  decoration: buildInputDecoration("Enter the password", Icons.password),
+                ),
+                SizedBox(height: 30,),
+                ElevatedButton(
+                  style: raisedButtonStyle,
+                  onPressed: () { },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?"),
+                    TextButton(
+                      onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => Signup()));
+                      },
+                      child: Text(
+                        "SignUp",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
-            TextField(
-              decoration: InputDecoration(labelText: "Password"),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: (){},
-              child: Text("Login"),
-            )
-          ],
+          ),
         ),
       ),
     );
--- lib/views/login_signup/signup.dart
+++ lib/views/login_signup/signup.dart
@@ -7,40 +7,27 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
+  InputDecoration buildInputDecoration(String labelText,) {
+    return InputDecoration(
+      border: UnderlineInputBorder(),
+      labelText: labelText,
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey),
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.blue, width: 2.0),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      // prefixIcon: Icon(icon, color: Colors.grey),
+      filled: true,
+      fillColor: Colors.grey.shade100,
+    );
+  }
   @override
   Widget build(BuildContext context) {
     final screen_width=MediaQuery.of(context).size.width;
     final screen_height=MediaQuery.of(context).size.height;
-    final InputDecoration emailDecoration=InputDecoration(
-      border: UnderlineInputBorder(),
-      labelText: 'Enter your username',
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.grey),
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.blue, width: 2.0),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      prefixIcon: Icon(Icons.email, color: Colors.grey),
-      filled: true,
-      fillColor: Colors.grey.shade100,
-    );
-    final InputDecoration passwordDecoration=InputDecoration(
-      border: UnderlineInputBorder(),
-      labelText: 'Enter the password',
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.grey),
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.blue, width: 2.0),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      prefixIcon: Icon(Icons.email, color: Colors.grey),
-      filled: true,
-      fillColor: Colors.grey.shade100,
-    );
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -57,15 +44,20 @@ class _SignupState extends State<Signup> {
             padding: EdgeInsets.all(20),
             child: Column(
               children: [
-                SizedBox(height: screen_height/5,),
+                SizedBox(height: screen_height/10,),
                 TextFormField(
-                  decoration:  emailDecoration,
+                  decoration: buildInputDecoration("Full Name"),
+                ),
+                SizedBox(height: screen_height/50,),
+                TextFormField(
+                  decoration: buildInputDecoration("Email"),
+                ),
+                SizedBox(height: screen_height/50,),
+                TextFormField(
+                  obscureText: true,
+                  decoration: buildInputDecoration("Password"),
                 ),
                 SizedBox(height: 20,),
-
-                TextFormField(
-                  decoration: passwordDecoration,
-                )
               ],
             ),
           ),
