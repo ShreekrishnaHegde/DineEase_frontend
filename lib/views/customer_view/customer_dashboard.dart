00e93d718803fa37ad9142cdf4a123e03b8cdf3e
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-07-04 14:55:26 +0530

    ADded dearch bar

--- lib/views/customer_view/customer_dashboard.dart
+++ lib/views/customer_view/customer_dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:dine_ease/models/HotelProfileModel.dart';
+import 'package:dine_ease/service/hotel_service/HotelProfileService.dart';
 import 'package:dine_ease/views/customer_view/customer_view_menu.dart';
 import 'package:flutter/material.dart';
 
@@ -11,26 +12,45 @@ class CustomerDashboard extends StatefulWidget {
 
 class _CustomerDashboardState extends State<CustomerDashboard> {
   final TextEditingController _searchController = TextEditingController();
-  List<HotelProfileModel> _results = [];
+  final HotelProfileService _hotelProfileService=HotelProfileService();
+  List<HotelProfileModel> _results=[];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Search Hotels"),
-      ),
-      body: Padding(
-       padding: const EdgeInsets.all(16),
-       child: Column(
-         children: [
-           TextField(
-             controller: _searchController,
-             // onChanged: _onSearchChanged,
-             decoration: InputDecoration(labelText: 'Hotels'),
-           ),
 
-         ],
-       ),
+      ),
+      backgroundColor: Colors.grey[100],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Hello ",
+                style: TextStyle(fontWeight: FontWeight.bold),
+
+              ),
+              const SizedBox(height: 20,),
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: 'Search for hotels...',
+                  prefixIcon: const Icon(Icons.search),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
